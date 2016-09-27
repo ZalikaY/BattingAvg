1e94f20ab63dd9783cf1897664aa79eba1493a14
Zalika Ockleberry <Ockleberryzalika@gmail.com>
2016-09-26 20:28:27 -0400

    First draft

---
+++ src/BattingQ.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BattingQ {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Counter for printing array
+		int i =0;
+		//Variable to store my input
+		int hits=0;
+		//Variable to store input in array
+		int numOfHits[]=new int[hits];
+			
+		System.out.println("Welcome to Batting Average Calculator!");
+		System.out.println("Enter number of times at bat:");
+		
+		int ab = sc.nextInt();
+		/*System.out.println("Enter the number of batters:");
+		int ba = sc.nextInt();*/
+		
+		int atBat[]=new int[ab];
+		
+		//To print an array 
+		for (i=0;i<atBat.length;i++){
+			
+			System.out.println("Result for at-bat " + (i+1));
+			
+			hits=sc.nextInt();
+			
+			if ()
+				
+			}
+		
+			
+			
+			
+			
+		
+		
+		}
+		}
+			
+
+	
+
+
